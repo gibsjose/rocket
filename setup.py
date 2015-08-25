94f51c568e70f0e18ef256656bacf3c1ce452d45
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-08-25 17:30:26 -0400

    Added init and setup scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rocket',
+    version='1.0',
+    description='Base Project Generator',
+    author='Joe Gibson',
+    author_email='gibsjose@mail.gvsu.edu',
+    url='https://github.com/gibsjose/Rocket',
+    packages=['rocket'],
+    )
